44072ea1a814edddfde1038e801ec06181b9fba4
sbx_user1051 <sbx_user1051@169.254.75.93>
2020-06-21 12:11:54 +0000

    Sun Jun 21 12:11:54 UTC 2020

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,18 +5,36 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        String result;
+        String result = String.valueOf(number);
+        Boolean three = false, five = false;
+        Integer copy = number;
+        int ok = 0;
 
-        if(number%3 == 0 && number%5 != 0)
+        while(copy!=0){
+            if(copy%10 == 3)
+                three = true;
+            if(copy%10 == 5)
+                five = true;
+            copy = copy/10;
+        }
+
+        if((number%3 == 0 && number%5 != 0) || (three && number%5 != 0))
+        {
             result = "fizz";
-        else if(number%3 != 0 && number%5 == 0)
-            result = "buzz";
-        else if(number%3 == 0 && number%5 == 0)
+            ok += 1;
+        }
+
+        if((number%3 != 0 && number%5 == 0) || (number%3 != 0 && five))
+           {
+               result = "buzz";
+               ok += 1;
+           }
+
+        if(ok == 2)
             result = "fizz buzz";
-        else
-            result = String.valueOf(number);
 
         return result;
     }
 
 }
+
